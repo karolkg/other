97f3af03334d9d9692b7f7ba6af88e6d2f5618a7
Karol Gajek <karl1@o2.pl>
2016-08-06 09:43:23 +0200

    new files

---
+++ src/Algoritms/BinaryGap.java
@@ -0,0 +1,35 @@
+package Algoritms;
+
+public class BinaryGap {
+  
+  public BinaryGap() {
+  }
+  
+  public int solution(int n) {
+    int counter = 0;
+    int gap = 0;
+    boolean flag = false;
+    int mask = 1;
+    
+    for(int i = 0; i < 32; i++) {
+      if((n & mask) == 0 && flag) {
+        counter += 1;        
+      }
+      
+      if((n & mask) == mask) {
+        flag = true;        
+        gap = gap > counter? gap: counter;
+        counter = 0;        
+      }      
+     mask =  mask << 1;
+    }
+    return gap;
+  }
+
+  public static void main(String[] args) {
+    BinaryGap bg = new BinaryGap();
+    int result = bg.solution(129);
+    System.out.println(result);
+  }
+
+}
---
+++ src/Algoritms/Fib.java
@@ -0,0 +1,26 @@
+package Algoritms;
+
+public class Fib {
+  String result;
+  public Fib() {  }
+  
+  public String countFib(int number) {
+    int l1 = 0;
+    int l2 = 1;
+    StringBuilder sb = new StringBuilder();
+    
+    for(int i=0; i < number; i++) {
+      sb.append(l1).append(" ");
+      int tmp = l2;
+      l2 = l1 + l2;
+      l1 = tmp;
+    }
+    result = sb.toString().trim();
+    return result;    
+  }
+  
+  public long countFibReq(int number) {   
+    if(number <= 1)  return number;    
+    return countFibReq(number-1) + countFibReq(number - 2);        
+  }
+}
