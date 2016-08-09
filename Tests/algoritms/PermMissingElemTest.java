eb2b7005d6865c41d51838a3c52565da8102630f
karolkg <karl1@o2.pl>
2016-08-09 18:36:09 +0200

    ver 0.1

--- Tests/algoritms/PermMissingElemTest.java
+++ Tests/algoritms/PermMissingElemTest.java
@@ -6,14 +6,16 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PermMissingElemTest {
+	PermMissingElem perm;
 
 	@Before
 	public void setUp() throws Exception {
+		perm = new PermMissingElem();
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals(2, perm.solution(new int[]{5,1,3,6,4,7}));
 	}
 
 }
--- src/algoritms/PermMissingElem.java
+++ src/algoritms/PermMissingElem.java
@@ -2,4 +2,19 @@ package algoritms;
 
 public class PermMissingElem {
 
+	public int solution(int[] A) {
+		int result = 0;
+		int B[] = new int[A.length + 1]; 
+		
+		for(int i = 0; i < A.length; i++) {
+			B[ A[i] - 1  ] = 1;
+		}
+		
+		for(int i = 1; i < B.length; i++) {
+			if(B[i] == 1) return i;
+		}
+		
+		return result;
+	}
+
 }
