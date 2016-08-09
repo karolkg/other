1a42c5f7d70e4d6c6735bde347a92add5d644309
karolkg <karl1@o2.pl>
2016-08-09 19:21:11 +0200

    final version 1.0

--- src/algoritms/PermMissingElem.java
+++ src/algoritms/PermMissingElem.java
@@ -4,14 +4,16 @@ public class PermMissingElem {
 
 	public int solution(int[] A) {
 		int result = 0;
+		if(A.length == 0) return 1;
+		
 		int B[] = new int[A.length + 1]; 
 		
 		for(int i = 0; i < A.length; i++) {
 			B[ A[i] - 1  ] = 1;
 		}
-		
-		for(int i = 1; i < B.length; i++) {
-			if(B[i] == 1) return i;
+				
+		for(int i = 0; i < B.length; i++) {
+			if(B[i] == 0) return i + 1;
 		}
 		
 		return result;
