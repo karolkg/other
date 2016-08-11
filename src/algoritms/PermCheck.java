fa5597aad4a7d9668e0e33c47e97841d1b3077b4
Karol <karl1@o2.pl>
2016-08-11 18:16:13 +0200

    Solution 1

--- src/algoritms/PermCheck.java
+++ src/algoritms/PermCheck.java
@@ -4,7 +4,17 @@ public class PermCheck {
   
   public int solution(int[] A) {
     int result = 0;
-      
+    int arrayLength = A.length;
+    int[] B = new int[arrayLength];
+    
+    for(int i = 0; i < arrayLength; i++) {
+      if(A[i] > arrayLength || A[i] < 1) return 0;
+      else B[A[i] - 1] = 1;      
+    }
+    
+    for(int i: B)
+      if(B[i] == 0) return 0;
+      else return 1;  
     
     return result;
   }
