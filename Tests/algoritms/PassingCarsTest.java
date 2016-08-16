7725194e0355a90bace986e5d341e9bce23af986
karolkg <karl1@o2.pl>
2016-08-16 20:00:04 +0200

    PassingCars finished

--- Tests/algoritms/PassingCarsTest.java
+++ Tests/algoritms/PassingCarsTest.java
@@ -14,8 +14,68 @@ public class PassingCarsTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPositive() {
+		int[] a = {0,1,0,1,0,1};
+		assertEquals(6, pc.solution(a));
+	}
+	
+	@Test
+	public void testPairsNumbersOverLimit() {
+		int[] a = new int[100000];
+		for(int i = 0; i < a.length; i++) {
+			if(i % 2 == 0) a[i] = 1;
+			else a[i] = 0;
+		}		
+		assertEquals(-1, pc.solution(a));
+	}
+	
+	@Test
+	public void testEmptyArray() {
+		int[] a = {};
+		assertEquals(0, pc.solution(a));
+	}
+	
+	@Test
+	public void testOneZeroElementArray() {
+		int[] a = {0};
+		assertEquals(0, pc.solution(a));
+	}
+	
+	@Test
+	public void testOneOneElementArray() {
+		int[] a = {1};
+		assertEquals(0, pc.solution(a));
+	}
+	
+	@Test
+	public void testZeroOneArray() {
+		int[] a = {0,1};
+		assertEquals(1, pc.solution(a));
+	}
+	
+	@Test
+	public void testOneZeroArray() {
+		int[] a = {1,0};
+		assertEquals(0, pc.solution(a));
+	}
+	
+	@Test
+	public void testArrayLimit() {
+		int[] a = new int[100000];
+		a[10] = 1;
+		assertEquals(10, pc.solution(a));
+	}
+	
+	@Test
+	public void tesArrayOverLimit() {
+		int[] a = new int[100001];
+		assertEquals(-1, pc.solution(a));
+	}
+	
+	@Test
+	public void testWrongValueInArray() {
+		int[] a = {0,2};		
+		assertEquals(-1, pc.solution(a));
 	}
 
 }
--- src/algoritms/PassingCars.java
+++ src/algoritms/PassingCars.java
@@ -2,12 +2,26 @@ package algoritms;
 
 public class PassingCars {
 	
-	public int solution(int[] A,int k, int m) {
-		int result = -1;
+	public int solution(int[] A) {
+		if(A.length > 100000) return -1;
 		
+		int result = 0;
+		int[] sumElem = new int[A.length + 1];
 		
+		//making a cumulative table
+		//if value in the table is different than 0 or 1 return -1
+		for(int i = 1; i < sumElem.length; i++) {
+			if(A[i - 1] < 0 || A[i - 1] > 1) return -1;
+			sumElem[i] = sumElem[i - 1] + A[i - 1];
+		}
 		
+		int sumAll = sumElem[ sumElem.length - 1];	
+		for(int i = 0; i < A.length; i++) {
+			if(A[i] == 0) {
+				result += sumAll - sumElem[i];
+				if(result > (long)1e9 || result < -1 ) return -1;
+			}
+		}		
 		return result;
 	}
-
 }
