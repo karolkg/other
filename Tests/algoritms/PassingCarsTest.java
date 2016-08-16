1546e5654c0a0e45ef067ea7332b4bc7f555b2a0
karolkg <karl1@o2.pl>
2016-08-16 17:30:10 +0200

    PassingCar class init

---
+++ Tests/algoritms/PassingCarsTest.java
@@ -0,0 +1,21 @@
+package algoritms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PassingCarsTest {
+	PassingCars pc;
+
+	@Before
+	public void setUp() throws Exception {
+		pc = new PassingCars();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/algoritms/PassingCars.java
@@ -0,0 +1,13 @@
+package algoritms;
+
+public class PassingCars {
+	
+	public int solution(int[] A,int k, int m) {
+		int result = -1;
+		
+		
+		
+		return result;
+	}
+
+}
