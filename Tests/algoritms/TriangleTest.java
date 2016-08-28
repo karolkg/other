7aaff2f8b67cd703772e47ec9a199968f2902b59
Karol <karl1@o2.pl>
2016-08-28 21:08:09 +0200

    done

--- Tests/algoritms/TriangleTest.java
+++ Tests/algoritms/TriangleTest.java
@@ -6,19 +6,55 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TriangleTest {
+  Triangle t;
 
   @Before
   public void setUp() throws Exception {
+    t = new Triangle();
   }
 
   @Test
-  public void testCorrectArray() {
-    fail("Not yet implemented");
+  public void testQuickSortArray() {
+    int[] a = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    t.solution(a);
+    assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9}, a);
   }
   
   @Test
   public void testNoTriangleInArray() {
-    fail("not implemented yet");
+    int[] a = {10, 5, 1, 20};
+    assertEquals(0, t.solution(a));
+  }
+  
+  @Test
+  public void testTriangleInArray() {
+    int[] a = {10, 5, 8};
+    assertEquals(1, t.solution(a));
+  }
+  
+  @Test
+  public void testEmptyArray() {
+    int[] a = {};
+    assertEquals(0, t.solution(a));
+  }
+  
+  @Test
+  public void test1ElementArray() {
+    int[] a = {1};
+    assertEquals(0, t.solution(a));
+  }
+  
+  @Test
+  public void test2ElementsArray() {
+    int[] a = {1,2};
+    assertEquals(0, t.solution(a));
+  }
+  
+  @Test
+  public void testMaxInt() {
+    int x = Integer.MAX_VALUE;
+    int[] a = {x,x,x};
+    assertEquals(1, t.solution(a));
   }
 
 }
--- src/algoritms/Triangle.java
+++ src/algoritms/Triangle.java
@@ -2,4 +2,45 @@ package algoritms;
 
 public class Triangle {
 
+  public int solution(int[] A) {
+    if(A.length < 3) return 0;
+    
+    int result = 0;
+    quicksort(A, 0, A.length-1);
+    
+    for(int i = 2; i < A.length; i++) {
+      long a = A[i - 2];
+      long b = A[i - 1];
+      long c = A[i];
+      if( a + b > c && a + c > b && b + c > a) return 1;      
+    }   
+    
+    return result;
+  }
+  
+  
+  private void quicksort(int[] array, int left, int right) {
+    int pivot = (left + right) / 2;
+    int value = array[pivot];
+    int i = left;
+    int j = right;
+    
+    do {
+      while(array[i] < value) i++;
+      while(array[j] > value) j--;
+      
+      if(i <= j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        i++;
+        j--;
+      }      
+    } while(i <= j);
+    
+    if(j < left) quicksort(array, left, j);
+    if(i > right) quicksort(array,i, right);
+    
+  }
+  
 }
